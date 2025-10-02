371817b4745df367f2a22bd3d3b0561020fadd92
Monroy-B <bryanmonroyaa@gmail.com>
2025-10-02 16:53:19 +0000

    Points output to correct output

--- integerDoubler.cpp
+++ integerDoubler.cpp
@@ -3,29 +3,24 @@
 #include <ios>
 
 void integerDoubler(std::string inputFile, std::string outputFile) {
-
     int input_int = 0;
+    double output_double = 0.0;
+
     std::ifstream in(inputFile, std::ios::binary);
     std::ofstream out(outputFile, std::ios::binary);
-    size_t i = 0;
+
+    char* input_int_ptr = reinterpret_cast<char*>(&input_int);
+    char* output_double_ptr = reinterpret_cast<char*>(&output_double);
+
+    //size_t i = 0;
     while(!in.eof()) {
-        ++i;
-        char* input_int_ptr = reinterpret_cast<char*>(&input_int);
-        
+        //++i;
         in.read(input_int_ptr, sizeof(int));
-
-        double output_double = static_cast<double>(input_int);
-
-
-        char* output_double_ptr = reinterpret_cast<char*>(&output_double);
+        output_double = static_cast<double>(input_int);
         out.write(output_double_ptr, sizeof(double));
     }
-    std::cout << "Loop ended!\n" << i << std::endl;
+    //std::cout << "Loop ended!\n" << i << std::endl;
 
     in.close();
     out.close();
 }
-
-/*int main() {
-    integerDoubler("./in.txt", "./out.txt");
-}*/
