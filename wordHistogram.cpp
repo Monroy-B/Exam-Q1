92341f7a908557bb7c6a17f672f6d3a3e3e84b12
Monroy-B <bryanmonroyaa@gmail.com>
2025-10-02 16:00:06 +0000

    Define wordHistogram Function

---
+++ wordHistogram.cpp
@@ -0,0 +1,33 @@
+#include "wordHistogram.hpp"
+
+std::vector<size_t> wordHistogram(std::string sentence) {
+    std::stringstream in(sentence);
+
+    std::vector<size_t> char_count;
+    
+    std::string word;
+
+    while (in >> word) {
+        //std::cout << word << std::endl;
+        size_t len = word.size();
+
+        while(char_count.size() < len + 1) {
+            //std::cout << "push back 0";
+            char_count.push_back(0);
+        }
+
+        char_count[len] += 1;
+        //std::cout << len << std::endl;
+    }
+
+    return char_count;
+}
+
+/*int main() {
+    std::vector<size_t> char_count = wordHistogram("I LOVEE BEANS.  asdf");
+
+    for (size_t i = 0; i < char_count.size(); ++i) {
+
+        std::cout << char_count[i] << std::endl;
+    }
+}*/
---
+++ wordHistogram.hpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+std::vector<size_t> wordHistogram(std::string sentence);
