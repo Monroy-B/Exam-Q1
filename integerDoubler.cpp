851cd066fee915d45fc19bd6a0cba8507ccfe477
Monroy-B <bryanmonroyaa@gmail.com>
2025-10-02 16:58:02 +0000

    Make function actually double input

--- integerDoubler.cpp
+++ integerDoubler.cpp
@@ -4,7 +4,7 @@
 
 void integerDoubler(std::string inputFile, std::string outputFile) {
     int input_int = 0;
-    double output_double = 0.0;
+    int output_double = 0;
 
     std::ifstream in(inputFile, std::ios::binary);
     std::ofstream out(outputFile, std::ios::binary);
@@ -16,11 +16,41 @@ void integerDoubler(std::string inputFile, std::string outputFile) {
     while(!in.eof()) {
         //++i;
         in.read(input_int_ptr, sizeof(int));
-        output_double = static_cast<double>(input_int);
-        out.write(output_double_ptr, sizeof(double));
+        output_double = input_int * 2;
+        out.write(output_double_ptr, sizeof(int));
     }
     //std::cout << "Loop ended!\n" << i << std::endl;
 
     in.close();
     out.close();
 }
+/*/
+int main() {
+    std::ofstream out("./in.txt", std::ios::binary);
+    int test = 1223;
+    out.write(reinterpret_cast<char*>(&test), sizeof(int));
+    out.write(reinterpret_cast<char*>(&test), sizeof(int));
+    out.write(reinterpret_cast<char*>(&test), sizeof(int));
+    out.write(reinterpret_cast<char*>(&test), sizeof(int));
+    out.close();
+    
+    integerDoubler("./in.txt", "./out.txt");
+
+    std::ifstream in("./out.txt", std::ios::binary);
+    double testd;
+    in.read(reinterpret_cast<char*>(&testd), sizeof(double));
+    std::cout << "double: " << testd << std::endl;
+    in.read(reinterpret_cast<char*>(&testd), sizeof(double));
+    std::cout << "double: " << testd << std::endl;
+    in.read(reinterpret_cast<char*>(&testd), sizeof(double));
+    std::cout << "double: " << testd << std::endl;
+    in.read(reinterpret_cast<char*>(&testd), sizeof(double));
+    std::cout << "double: " << testd << std::endl;
+    /*in.read(reinterpret_cast<char*>(&testd), sizeof(int));
+    std::cout << "int: " << testd << std::endl;
+    in.read(reinterpret_cast<char*>(&testd), sizeof(int));
+    std::cout << "int: " << testd << std::endl;
+    in.read(reinterpret_cast<char*>(&testd), sizeof(int));
+    std::cout << "int: " << testd << std::endl;*/
+
+}*/
